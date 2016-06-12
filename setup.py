4ee7f2df1f76259defd0eb53a2825d65decac550
Abu Ashraf Masnun <masnun@gmail.com>
2016-06-13 04:20:38 +0600

    Bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='urt',
-    version='0.1.1',
+    version='0.1.2',
     description='Urban Terror Server Monitor',
     url='http://github.com/masnun/urt',
     author='Abu Ashraf Masnun',
