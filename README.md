5af18c76b4ec665c30aeb0eb07ad21cdba65b156
Abu Ashraf Masnun <masnun@gmail.com>
2016-06-13 04:17:36 +0600

    Added README

---
+++ README.md
@@ -0,0 +1,13 @@
+## Urban Terror Server Monitor
+
+It's a handy tool to monitor the players on a certain UrT server. 
+
+
+#### Installation
+
+    pip install urt
+
+#### Usage
+
+    urt 5.135.165.34 27001
+
--- setup.py
+++ setup.py
@@ -2,15 +2,16 @@ from setuptools import setup
 
 setup(
     name='urt',
-    version='0.1',
+    version='0.1.1',
     description='Urban Terror Server Monitor',
     url='http://github.com/masnun/urt',
     author='Abu Ashraf Masnun',
     author_email='masnun@gmail.com',
     license='MIT',
-    packages=['pyurt'],
+    packages=['urt'],
     install_requires=[
         'urwid',
     ],
-    scripts=['pyurt/urt.py']
+    scripts=['urt/urt'],
+    long_description=open("README.md").read()
 )
